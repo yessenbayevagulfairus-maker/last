e9a09a52fed488a43681907c10c3e2a5dece45a1
yessenbayevagulfairus-maker <yessenbayeva.gulfairus@gmail.com>
2026-05-09 12:56:02 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-
+const CACHE = 'gulfairus-v2';
+const BASE = self.registration.scope;
+const FILES = [BASE, BASE + 'index.html', BASE + 'manifest.json', BASE + 'icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match(BASE + 'index.html'))));
+});
